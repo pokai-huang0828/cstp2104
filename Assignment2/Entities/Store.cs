2dc383a2b153b964ab7f19d80e74587b35bc942a
pokai-huang0828 <000429977@student.vcc.ca>
2022-01-20 00:36:35 -0800

    Added store to A2

--- Assignment2/Entities/Store.cs
+++ Assignment2/Entities/Store.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace Assignment2.Entities
 {
-    class Store
+    public class Store
     {
-        
+        public List<Car> CarList { get; set; }
+        public List<Motorcycle> MotorcycleList { get; set; }
+        public List<Truck> TruckList { get; set; }
+
+        public Store()
+        {
+            CarList = new List<Car>();
+            MotorcycleList = new List<Motorcycle>();
+            TruckList = new List<Truck>();
+        }
+
+
     }
 }
